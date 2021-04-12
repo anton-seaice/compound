81e477b9c925e4b547e6f8fd63cff5a7c41dd95b
anton-climate <79179784+anton-climate@users.noreply.github.com>
2021-04-12 15:05:12 +1000

    Update cvdpTime.py
    Updated based on https://github.com/nickywright/palaeoclimate-lastmillennium/blob/master/CESM-LME%20exploration/CESM-LME%20and%20Ni%C3%B1o3.4%20-%20using%20xarray.ipynb?short_path=7c577c5

--- helpers/cvdpTime.py
+++ helpers/cvdpTime.py
@@ -1,12 +1,23 @@
 #This is multi-dimensional array handling
 import xarray
 
-import pandas
+import cftime
 
-def decode(xa):
+from dateutil.relativedelta import *
+
+def decodeTime(ds):
     
-    if xa.time.units == 'months since 850-01-15 00:00:00':
-        xa.time['date'] = pandas.period_range(start='850-01-15', periods=len(xa.time), freq='M')
+    if ds.time.units == 'months since 850-01-15 00:00:00':
+        newDate=[]
+        startDate=datetime(850, 1,15)
+        for i in ds.time.values:
+            newDate.append(startDate+relativedelta(months=+int(i))
+        
+       decodedTimes=cftime.date2num(newDate, 'days since 850-01-15 00:00:00', calendar='standard') 
+       attrs = {'units': 'days since 850-01-15 00:00:00', 'calendar': 'standard'}
+       dates = xarray.Dataset({'time': ('time', decodedTimes, attrs)})
+       dates = xarray.decode_cf(dates)
+       ds.update({'time':('time', dates['time'], attrs)})
     
     return xa
 
