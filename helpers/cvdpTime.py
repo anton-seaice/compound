d68afe28af569e00bcfc51f698359d065b8730ec
anton-climate <anton.steketee@anu.edu.au>
2021-04-12 17:24:30 +1000

    first pass date handling for cvdp

--- helpers/cvdpTime.py
+++ helpers/cvdpTime.py
@@ -1,23 +1,16 @@
 #This is multi-dimensional array handling
 import xarray
+import numpy
 
-import cftime
 
-from dateutil.relativedelta import *
+
 
 def decodeTime(ds):
-    
-    if ds.time.units == 'months since 850-01-15 00:00:00':
-        newDate=[]
-        startDate=datetime(850, 1,15)
-        for i in ds.time.values:
-            newDate.append(startDate+relativedelta(months=+int(i))
+    # Ref http://xarray.pydata.org/en/stable/weather-climate.html
+
+    dates = xarray.cftime_range(start="0850", periods=13872, freq="MS", calendar="noleap")
+       
+    ds=ds.assign_coords(time=("time",dates))
         
-       decodedTimes=cftime.date2num(newDate, 'days since 850-01-15 00:00:00', calendar='standard') 
-       attrs = {'units': 'days since 850-01-15 00:00:00', 'calendar': 'standard'}
-       dates = xarray.Dataset({'time': ('time', decodedTimes, attrs)})
-       dates = xarray.decode_cf(dates)
-       ds.update({'time':('time', dates['time'], attrs)})
-    
-    return xa
+    return ds
 
