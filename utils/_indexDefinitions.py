ba6506e000d6dd81928cf4cfbb23b841994f7f89
anton-climate <anton.steketee@anu.edu.au>
2021-04-19 17:00:32 +1000

    new file for index defitions

---
+++ utils/_indexDefinitions.py
@@ -0,0 +1,10 @@
+
+
+sstIndex = {
+    "nino12" : {"latMin":-10, "latMax":0, "longMin":270,"longMax":280} ,
+    "nino3" : {"latMin":-5, "latMax":5, "longMin":210,"longMax":270},
+    "nino34" : {"latMin":-5, "latMax":5, "longMin":190,"longMax":240},
+    "nino4" : {"latMin":-5, "latMax":5, "longMin":160,"longMax":210},
+    "westIO" : {"latMin":-10, "latMax":10, "longMin":50,"longMax":70},
+    "eastIO" : {"latMin":-10, "latMax":0, "longMin":90,"longMax":110}
+} 
