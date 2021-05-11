23b3cf8d5e2d3275ea294189defb5cbd0f0992d3
anton-climate <anton.steketee@anu.edu.au>
2021-05-11 10:45:33 +1000

    Adjustable climatology for sst indeces

--- utils/pslIndex.py
+++ utils/pslIndex.py
@@ -9,7 +9,7 @@ import xarray
 
 
 
-def calculateSamIndex(ds, climatStart, climatFinish):
+def calculateSamIndex(ds, climatStart, climatFinish, *args):
     
     """
     This function calculates sam index based on monthly climatology.
@@ -25,20 +25,42 @@ def calculateSamIndex(ds, climatStart, climatFinish):
         
     """
     
+    if len(args)>1:
+        raise(EnvironmentError("Too many input arguments provided"))
+    
     #The two latitudes of interest are defined in _indexDefinitions
     domain = _index.pslIndex['sam']
     
     #ds for output
     samIndex=xarray.Dataset(coords={"time":ds.time})
+    
+    for keys in domain:
+        domainDs[keys]=ds.sel(lat=domain['lat1'],method='nearest', drop=True).PSL.mean(dim='lon')
+        climatologyDs[keys]=climat.dateInterval(ds40, climatStart, climatFinish)
 
+        """
     #average across each latitude of interest
     ds40=ds.sel(lat=domain['lat1'],method='nearest', drop=True).PSL.mean(dim='lon')
     ds65=ds.sel(lat=domain['lat2'],method='nearest', drop=True).PSL.mean(dim='lon')
     
-    #filter by the climatology
-    ds40Climatology = climat.dateInterval(ds40, climatStart, climatFinish)
-    #filter by the climatology
-    ds65Climatology = climat.dateInterval(ds65, climatStart, climatFinish)
+    if len(args)==0:
+        #filter by the climatology
+        ds40Climatology = climat.dateInterval(ds40, climatStart, climatFinish)
+        #filter by the climatology
+        ds65Climatology = climat.dateInterval(ds65, climatStart, climatFinish)
+    elif len(args)==1:
+        regex=re.compile('xarray')
+        if regex.search(str(type(args[0])))!=None:
+            climatDs=args[0]
+            
+            climatDs40=climatDs.sel(lat=domain['lat1'],method='nearest', drop=True).PSL.mean(dim='lon')
+            climatDs65=climatDs.sel(lat=domain['lat2'],method='nearest', drop=True).PSL.mean(dim='lon')
+            
+            ds40Climatology = climat.dateInterval(climatDs40, climatStart, climatFinish)
+            ds65Climatology = climat.dateInterval(climatDs65, climatStart, climatFinish)
+    else:
+        raise(EnvironmentError("Climatology Ds provided is not an xarray"))
+    """
     
     #normalise each latitude using these climatologies and calculate the difference
     samIndex['sam']=climat.normalise(ds40,ds40Climatology)-climat.normalise(ds65,ds65Climatology)
--- utils/sstIndex.py
+++ utils/sstIndex.py
@@ -1,5 +1,6 @@
 import xarray
 import cftime
+import re
 
 import sys
 sys.path.append('../')
@@ -23,21 +24,33 @@ def sstDomain(ds, indexKey):
 
     return domainDs
 
-def calculateIndex(ds, climatStart, climatFinish):
+def calculateIndex(ds, climatStart, climatFinish, *args):
     """
     This function calculates an area-average for the indeces specified in _indexDefitions based on monthly climatology.
     
     inputs:
     
-    xarray ds in CESM format
+    xarray ds to use for index calculationin CESM format
     start and finish years to use for climatology
-    
+
+    *args can be used for an xarray climatDs to use for climatology (if different to the main dataset)
+
     output:
     
     new xarray with time dimension
         
     """
 
+    if len(args)==0:
+        climatDs=ds.copy()
+    elif len(args)==1:
+        regex=re.compile('xarray')
+        if regex.search(str(type(args[0])))!=None:
+            climatDs=args[0]
+        else:
+            raise(EnvironmentError("Climatology Ds provided is not an xarray"))
+    else:
+        raise(EnvironmentError("Too many input arguments provided"))
     
     # For now, assume this is CESM output. Although CMIP should be principally the same non?
     #list of index names, as defined in _indexDefitions file
@@ -45,8 +58,9 @@ def calculateIndex(ds, climatStart, climatFinish):
     
     #There's only one depth dimension, so we will drop that
     ds['SST']=ds.SST.isel(z_t=0)
-
-    #For some reason TAREA has a time demension, but doesn't change in time, so well drop that too
+    climatDs['SST']=climatDs.SST.isel(z_t=0)
+    
+    #For some reason TAREA has a time dimension, but doesn't change in time, so well drop that too
     ds['TAREA']=ds.TAREA.isel(time=0)
 
     #Making TAREA a coordinate
@@ -59,9 +73,11 @@ def calculateIndex(ds, climatStart, climatFinish):
     for key in index:
         
         domainDs=sstDomain(ds, key)
+        
+        domainClimatDs=sstDomain(climatDs, key)
 
         # First calculate the data range to use for climatology 
-        domainSstClimat=climat.dateInterval(domainDs, climatStart, climatFinish)
+        domainSstClimat=climat.dateInterval(domainClimatDs, climatStart, climatFinish)
         
         #calculate the monthly means of that range
         sstMean = domainSstClimat.groupby('time.month', restore_coord_dims=True).mean(dim='time')
