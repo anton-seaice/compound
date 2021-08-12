66844ae580551eb41cd994c21576b0d882dc69c0
anton-climate <anton.steketee@anu.edu.au>
2021-08-12 18:57:27 +1000

    tweaks to area of interest map

--- utils/_indexDefinitions.py
+++ utils/_indexDefinitions.py
@@ -1,7 +1,7 @@
 
 # This dict is a container for the areas to calculate SST anomalies for when calculting SST indeces
 sstIndex = {
-    "nino12" : {"latMin":-10, "latMax":0, "longMin":270,"longMax":280} ,
+    "nino1+2" : {"latMin":-10, "latMax":0, "longMin":270,"longMax":280} ,
     "nino3" : {"latMin":-5, "latMax":5, "longMin":210,"longMax":270},
     "nino34" : {"latMin":-5, "latMax":5, "longMin":190,"longMax":240},
     "nino4" : {"latMin":-5, "latMax":5, "longMin":160,"longMax":210},
